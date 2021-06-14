314178aa79b7a1fe9f52e78277b2da35770b1684
alleguiemna <allegui.emna@yahoo.fr>
2021-06-14 11:26:49 +0200

    Create a React app and publish it to GitHub Pages

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {HashRouter} from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 ReactDOM.render(
   <React.StrictMode>
-  <HashRouter  basename={process.env.PUBLIC_URL}>
+  <Router  >
     <App />
-  </HashRouter>
+  </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
