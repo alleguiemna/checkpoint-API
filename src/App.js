67bbde66c548cc11b791d2797bd861f7db77bf59
alleguiemna <allegui.emna@yahoo.fr>
2022-03-15 21:29:07 +0100

    latest commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import UserList from "./UserList";
-import { Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import UserDetail from "./UserDetail";
 import Header from "./Header";
 import Footer from "./Footer";
@@ -10,14 +10,14 @@ function App() {
   return (
     <div className="App">
       <Header  />
-     
-         <Switch> 
+      <Router>
+      <Switch> 
           <Route path="/" exact component={UserList} />
           <Route
             path="/profile/:id" 
             render={(props) => <UserDetail {...props}/>} />
-         </Switch>
-      
+      </Switch>
+      </Router>
        <Footer /> 
     </div>
   );
